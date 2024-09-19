6ea6960f04e4b0b2e5264e661bf3057fc2c332fc
Db_27YT <disputedbowl@gmail.com>
2024-09-19 22:35:47 +0200

    move script added to move files locally

---
+++ move.py
@@ -0,0 +1,4 @@
+import shutil
+
+shutil.copy("C:/Users/Josua/Documents/ComputerCraft/Vote System/serverVote.lua","C:/Users/Josua/AppData/Roaming/CraftOS-PC/computer/0/serverVote.lua" )
+shutil.copy("C:/Users/Josua/Documents/ComputerCraft/Vote System/clientVote.lua","C:/Users/Josua/AppData/Roaming/CraftOS-PC/computer/1/clientVote.lua")
