410bba81889b2422f4a32c429fc3ba2a22d68a6d
Db_27YT <disputedbowl@gmail.com>
2024-10-27 21:42:47 +0000

    aa

--- WWAS/WWAS-Client.lua
+++ WWAS/WWAS-Client.lua
@@ -7,10 +7,10 @@ function ReceiveAlarm()
     local id, message = rednet.receive(WWAS_AUDIO_PROTOCOL)
     local id, text1 = rednet.receive(WWAS_TEXT_PROTOCOL)
     local id, text2 = rednet.receive(WWAS_TEXT_PROTOCOL)
-    DisplayAlarm(message,text1)
+    DisplayAlarm(message,text1,text2)
 end
 
-function DisplayAlarm(message,text1)
+function DisplayAlarm(message,text1,text2)
     local monitor = peripheral.find("monitor")
     if message then
         redstone.setOutput("right",true)
