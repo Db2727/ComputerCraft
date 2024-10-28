7d9f58ae031dea23fd938530b86a39b538d86475
Db_27YT <disputedbowl@gmail.com>
2024-10-28 00:50:01 +0000

    added speaker support

--- WWAS/WWAS-Client.lua
+++ WWAS/WWAS-Client.lua
@@ -1,20 +1,46 @@
 WWAS_AUDIO_PROTOCOL = "WWAS_AUDIO"
 WWAS_TEXT_PROTOCOL = "WWAS_TEXT"
 WWAS_STOP_PROTOCOL = "WWAS_STOP"
+WWAS_ID = "NULL"
 
 
 function ReceiveAlarm()
+    local monitor = peripheral.find("monitor")
+    monitor.clear()
+    monitor.setCursorPos(1,4)
+    monitor.write(WWAS_ID)
+    monitor.setCursorPos(1,1)
+
     rednet.open("front")
     local id, message = rednet.receive(WWAS_AUDIO_PROTOCOL)
-    local id, text1 = rednet.receive(WWAS_TEXT_PROTOCOL)
-    local id, text2 = rednet.receive(WWAS_TEXT_PROTOCOL)
-    DisplayAlarm(message,text1,text2)
-end
+    if not message then
+        if message == WWAS_ID then
+            local id, mode = rednet.receive(WWAS_AUDIO_PROTOCOL)
+            local id, sound = rednet.receive(WWAS_AUDIO_PROTOCOL)
+            local id, text1 = rednet.receive(WWAS_TEXT_PROTOCOL)
+            local id, text2 = rednet.receive(WWAS_TEXT_PROTOCOL)
+            DisplayAlarm(message,sound,mode,text1,text2)
+        else
+            ReceiveAlarm()
+        end
+    else
+        local id, mode = rednet.receive(WWAS_AUDIO_PROTOCOL)
+        local id, sound = rednet.receive(WWAS_AUDIO_PROTOCOL)
+        local id, text1 = rednet.receive(WWAS_TEXT_PROTOCOL)
+        local id, text2 = rednet.receive(WWAS_TEXT_PROTOCOL)
+        DisplayAlarm(message,mode,sound,text1,text2)
+    end
+end 
 
-function DisplayAlarm(message,text1,text2)
+function DisplayAlarm(message,mode,sound,text1,text2)
     local monitor = peripheral.find("monitor")
     if message then
-        redstone.setOutput("right",true)
+        if mode == "siren" then
+            redstone.setOutput("right",true)
+        elseif mode == "speaker" then
+            local speaker = peripheral.find("speaker")
+            PlaySoundFile(sound)
+        end
         monitor.clear()
         monitor.setCursorPos(1,1)
         monitor.write(text1)
@@ -40,7 +66,19 @@ function StopAlarm()
     end
 end
 
+function PlaySoundFile(filename)
+    local dfpwm = require("cc.audio.dfpwm")
+    local speaker = peripheral.find("speaker")
 
+    local decoder = dfpwm.make_decoder()
+    for chunk in io.lines("data/" ..filename, 16 * 1024) do
+        local buffer = decoder(chunk)
+
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
+    end
+end
 
 local monitor = peripheral.find("monitor")
 monitor.clear()
--- WWAS/WWAS-Server.lua
+++ WWAS/WWAS-Server.lua
@@ -4,37 +4,44 @@ WWAS_STOP_PROTOCOL = "WWAS_STOP"
 MODE = "test" --(test, nuclear, power, stop, custom)
 
 
+
+
 function ReadNuclear()
     local input = false
     while not input do
+        sleep(0.1)
         input = redstone.getInput("right")
     end
     
     if input then
-        SendAlarm("NUCLEAR EMERGENCY", "PLEASE STAY CALM")
+        SendAlarm("siren", false, "NUCLEAR EMERGENCY", "PLEASE STAY CALM")
     end
-    Activate()
 end
 
 function TestAlarm()
     local input = false
     while not input do
+        sleep(0.1)
         input = redstone.getInput("front")
     end
     if input then
-        SendAlarm("THIS IS A WWAS TEST", "DO NOT PANIC")
+        SendAlarm("speaker", "announce", "THIS IS A WWAS TEST", "DO NOT PANIC")
     end
     Activate()
 end
 
 function PowerLoss()
-    SendAlarm("BLACKOUT ON MAINGRID", "PLEASE STAY CALM")
+    SendAlarm("siren", false, "BLACKOUT ON MAINGRID", "PLEASE STAY CALM")
 end
 
 
-function SendAlarm(text1, text2)
+function SendAlarm(mode,sound, text1, text2)
     rednet.open("top")
     rednet.broadcast(true, WWAS_AUDIO_PROTOCOL)
+    sleep(0.5)
+    rednet.broadcast(mode,WWAS_AUDIO_PROTOCOL)
+    sleep(0.5)
+    rednet.broadcast(sound,WWAS_AUDIO_PROTOCOL)
     sleep(0.5)
     rednet.broadcast(text1, WWAS_TEXT_PROTOCOL)
     sleep(0.5)
@@ -45,6 +52,7 @@ function StopAlarm()
     rednet.open("top")
     local input = false
     while not input do
+        sleep(0.1)
         input = redstone.getInput("front")
     end
     if input then
