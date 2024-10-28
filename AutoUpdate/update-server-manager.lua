872593dee3a9a463d43aa80515404d34c9804792
Db_27YT <disputedbowl@gmail.com>
2024-10-28 17:54:27 +0000

    fixes

--- AutoUpdate/update-server-manager.lua
+++ AutoUpdate/update-server-manager.lua
@@ -1,15 +1,16 @@
 UPDATE_MANAGER_PROTOCOL = "UPDATE_MANAGER_PROTOCOL"
-ID_LIST = {1,2,3,4}
+UPDATE_PROTOCOL = "UPDATE"
+ID_LIST = {25,26,27,28,29}
 
-Active_List = {false,false,false,false}
+Active_List = {false,false,false,false,false}
 
 
 function ManageServers()
     rednet.open("top")
-    local id,message = rednet.receive(UPDATE_MANAGER_PROTOCOL)
+    local id,request = rednet.receive(UPDATE_MANAGER_PROTOCOL)
 
-    if message == "active" then
-        for i,v in ipairs(ID_LIST)
+    if request == "active" then
+        for i,v in ipairs(ID_LIST) do
             if v == id then
                 Active_List[i] = true
             end
@@ -24,9 +25,9 @@ function ManageServers()
                 local server_id = ID_LIST[i]
 
                 rednet.send(server_id,id,UPDATE_MANAGER_PROTOCOL)
-                id,message = rednet.receive(UPDATE_MANAGER_PROTOCOL)
+                local id,message = rednet.receive(UPDATE_MANAGER_PROTOCOL)
                 if message then
-                    rednet.send(server_id,message,UPDATE_MANAGER_PROTOCOL)
+                    rednet.send(server_id,request,UPDATE_PROTOCOL)
                 end
                 
             else
