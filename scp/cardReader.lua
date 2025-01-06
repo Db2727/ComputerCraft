a39770391520d53db0a4195c555578fdc490d086
Db_27YT <disputedbowl@gmail.com>
2025-01-06 08:08:11 +0000

    yes

--- scp/cardReader.lua
+++ scp/cardReader.lua
@@ -1,21 +1,47 @@
 local LEVEL = 1
-local OUTPUT = "right"
+local PLAYER_DETECTOR_NUM = "1"
+local READ_PLAYER = true
+local OUTPUT = "left"
+
+
 
 local card = peripheral.find("magnetic_card_manipulator")
-
+local player = peripheral.find("playerDetector_" ..PLAYER_DETECTOR_NUM)
 
 local function readCard()
-    local data = card.readCard()
-    if tonumber(data) == LEVEL then
-        redstone.setOutput(OUTPUT, true)
-        card.ejectCard()
-        os.sleep(5)
-        redstone.setOutput(OUTPUT,false)
-    else
-        card.ejectCard()
+    if card.hasCard() then
+        local data = card.readCard()
+        if tonumber(data) >= LEVEL then
+            if READ_PLAYER then
+                local x = ReadPlayer()
+                if x then
+                    print("no no no")
+                else
+                    print("yes yes yes")
+                end
+            end
+            redstone.setOutput(OUTPUT, true)
+            os.sleep(5)
+            redstone.setOutput(OUTPUT,false)
+        end
     end
-
     readCard()
+end
+
+for i = 1, 10, 1 do
+    
+end
+
+function ReadPlayer()
+    rednet.open("front")
+    local event, username, device = os.pullEvent("playerClick")
+    rednet.send(49,username)
+    local id,message = rednet.receive()
+    if message then
+        return true
+    else
+        return false
+    end
 end
 
 readCard()
--- scp/cardServer.lua
+++ scp/cardServer.lua
@@ -1,1 +1,17 @@
 local BANNED_PLAYERS = {"Db_27YT"}
+rednet.open("top")
+
+local function main()
+    local x = false
+    local id,message = rednet.receive()
+    for index, value in ipairs(BANNED_PLAYERS) do
+        if value == message then
+            x = true
+        end
+    end
+    x = false
+    rednet.send(id,x)
+    main()
+end
+
+main()
